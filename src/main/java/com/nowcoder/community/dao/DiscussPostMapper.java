18cdf133171ae4db3e926c68c25161151466a8b9
lllvpeng <657547263@qq.com>
2021-04-22 21:35:05 +0800

    第三章完成

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -9,10 +9,16 @@ import java.util.List;
 @Mapper
 public interface DiscussPostMapper {
 
-    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+    List<DiscussPost> selectDiscussPosts(@Param("userId")int userId,@Param("offset") int offset, @Param("limit")int limit);
 
     // @Param注解用于给参数取别名,
     // 如果只有一个参数,并且在<if>里使用,则必须加别名.
     int selectDiscussPostRows(@Param("userId") int userId);
+
+    int insertDiscussPost( DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
+
+    int updateCommentCount(@Param("id") int id,@Param("commentCount") int commentCount);
 
 }
